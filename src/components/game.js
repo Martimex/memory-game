2d42f2762d341ece0a4af910422f0bebad243171
Martimex <kamilmarciniak533@wp.pl>
2021-07-31 20:44:59 +0200

    Resolve repeatadly generated icons every single render,  fixed an issue with wrong move counting, get rid of misleading console errors

--- src/components/game.js
+++ src/components/game.js
@@ -1,5 +1,5 @@
 
-import  React, { useState, useEffect, useRef } from 'react';
+import  React, { useState, useEffect, useLayoutEffect, useRef } from 'react';
 import '../styles/game.css';
 import  levels from '../levels.js';
 
@@ -10,6 +10,8 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 import GameInfo from './game_info.js';
 
 import { setIcon, fasArray, fabArray } from './landing.js';
+import { set } from 'animejs';
+import { render } from '@testing-library/react';
 
 library.add(fab, fas);
 
@@ -75,14 +77,19 @@ function setRandomIcons(fasArray, usedIcons, randomizedIcons, tiles) {
 
 function Game(props) {
 
+    const [renderCount, setRenderCount] = useState(0);
     const [tiles, setTiles] = useState(null);
     const [level, setLevel] = useState(1);
     const [score, setScore] = useState(0);
     const [moves, setMoves] = useState(0);
     const [time, setTime] = useState(null);
 
-    // INIT
-    setRandomIcons(fasArray, usedIcons, randomizedIcons, tiles);
+    //  Render Count pomaga pozbyć się mylących błędów z konsoli - zmienna pilnuje, czy render wykonał się 1 raz. Jeśli ma się on wykonać po raz
+    //  kolejny, to nie tworzymy na nowo tabeli z ikonkami (unikamy podmiany ikon na planszy podczas gry)
+    if(renderCount < 1) {
+        setRandomIcons(fasArray, usedIcons, randomizedIcons, tiles);
+        setRenderCount(renderCount + 1);
+    }
 
     const changeTileNumber = () => {
 
@@ -90,24 +97,27 @@ function Game(props) {
         setLevel(level + 1);
         // setTiles(0); // CHANGE TO 10 LATER
         setTiles(levels[`lvl${level}`].tiles);
+        setRenderCount(0);
+
+        cleanup();
+    }
+
+    const cleanup = () => {
+        // Tu będzie rotate wszystkich ikon; w skrócie: przywracamy poziom do stanu pierwotnego i potem tworzymy kolejny poziom
+        // Spróbuj jeszcze pomyśleć nad prerobieniem mechanizmu dla arr (L: 124 - 134); być może to wystarczy i ta funkcja okaże się bez sensu
     }
 
     const gameboard = useRef(null);
 
-    useEffect(() => {
-        const board = gameboard.current;
-
-        board.addEventListener('click', (e) => {
-            if(e.target.classList.contains('tile')) {
-                console.log('event fired');
-                e.target.style = 'transform: rotateY(180deg)';
-                setMoves(moves + 1);
-            } else {
-                console.log('x');
-            }
-
-        })
-    }, [])
+    const clickable = (e) => {
+        if(e.target.classList.contains('tile')) {
+            console.log('tile detected');
+            e.target.style = 'transform: rotateY(180deg)';
+            setMoves(moves + 1);
+        } else {
+            console.log('nope');
+        }
+    }
 
     console.log(tiles);
 
@@ -127,14 +137,39 @@ function Game(props) {
 
     let allTiles =  arr.map((tile, index) => 
         <div className={`tile`} key={index.toString()}><div className='tile-front'></div> <div className='tile-back'>{<FontAwesomeIcon icon={`${randomizedIcons[index]}`} className={`fa-icon`}/>}</div></div>
-    );
+    ); 
 
-    useEffect((allTiles) => {
-        allTiles = arr.map((tile, index) => 
+    /* function createAllTiles(randomizedIcons) {
+        let allTiles = arr.map((tile, index) => 
             <div className={`tile`} key={index.toString()}><div className='tile-front'></div> <div className='tile-back'>{<FontAwesomeIcon icon={`${randomizedIcons[index]}`} className={`fa-icon`}/>}</div></div>
         );
+
         return allTiles;
-    }, [level])
+    } */
+
+    /* useLayoutEffect(() => {
+
+        console.log('useLayoutEffect');
+
+        //setRandomIcons(fasArray, usedIcons, randomizedIcons, tiles);
+        //createAllTiles(randomizedIcons);
+
+       allTiles = arr.map((tile, index) => 
+            <div className={`tile`} key={index.toString()}><div className='tile-front'></div> <div className='tile-back'>{<FontAwesomeIcon icon={`${randomizedIcons[index]}`} className={`fa-icon`}/>}</div></div>
+        );
+
+       const board = gameboard.current;
+       //let div = <p> Hi there </p>
+       //board.appendChild(div);
+    }, [level]) */
+
+    console.log(allTiles);
+
+   /* if(allTiles.length <= 0) {
+        console.log('null 123');
+        return null;
+    }
+    */
 
     return(
         <div>
@@ -148,7 +183,7 @@ function Game(props) {
 
                 <div> {levels[`lvl${level-1}`].lv} poziom zawiera {levels[`lvl${level-1}`].tiles} kafelków - Kolumny: {levels[`lvl${level-1}`].columns}; </div>
                 <div className='game'>
-                    <div className='board' ref={gameboard}>
+                    <div className='board' ref={gameboard} onClick={clickable}>
                         {allTiles}
                     </div>
                 </div>
