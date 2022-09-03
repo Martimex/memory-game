47806807e125a12ececa13eaa29ec3ed278137cf
Martimex <kamilmarciniak533@wp.pl>
2022-09-03 21:28:16 +0200

    Prepare dynamic background for specific level info
    This commit include just one, albeit important change:
    
    1. For each level, a corresponding background is shown. Player can see
    background image of level he chose. Levels that does not have SVG-type
    background (just linear / radial - gradient), have a fallback -
    prepared object of their custom gradients.
    
    Each background loads asynchronously, meaning that once user opens up
    level details tab, background loads in the background. The moment it is
    loaded, a fadeIn animation is fired, which shows up the level background
    in good UX way. It is no longer the issue, that the loaded background
    flashes immediately, causing weird effect for the user.

--- src/components/level_info.js
+++ src/components/level_info.js
@@ -1,21 +1,65 @@
 import '../styles/level_info.css';
+import { background_gradients } from '../global/exceptions/background_gradients.js';
+import React, { useEffect, useRef } from 'react';
 import { faStar as star_full, faCheck as check, faFileAlt as notes, faChartBar as stats, faPlay as play } from '@fortawesome/free-solid-svg-icons';
 import { faStar as star_empty} from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import anime from 'animejs';
 
 function LevelInfo(props) {
 
+    const bg_placeholder_ref = useRef(null);
+
     function checkCloseCondition(e) {
-        //console.log(e.target.classList);
         if(e.target.classList.contains('level-info-box')) {
             props.closeLevelInfo([null, null]);
         }
     }
 
+    function loadBackground_preview(url) {
+        return new Promise((resolve, reject) => {
+            const bg_image = new Image();
+            bg_image.addEventListener('load', () => {
+                resolve(bg_image);
+            })
+            bg_image.addEventListener('error', (err) => {reject(err)});
+
+            bg_image.src = url;
+        })
+
+    }
+
+    useEffect(() => {
+       const img_url = `bgs/${props.serie_name}/bg-${props.level_details.number}.svg`;
+        loadBackground_preview(img_url)
+            .then(() => {
+                bg_placeholder_ref.current.style.backgroundImage = `url(${img_url})`;
+                anime({
+                    targets: bg_placeholder_ref.current,
+                    duration: 500,
+                    opacity: [0, 1],
+                    easing: 'linear',
+                })
+            })
+            .catch((error) => { console.log(error); 
+                // + Handle exceptions for TF serie levels (1 to 6), because they only have default SVG backgrounds, but CSS gradients
+                if((props.serie_name === 'tf') && (parseInt(props.level_details.number) <= 6)) {
+                    bg_placeholder_ref.current.style.backgroundImage = background_gradients[props.serie_name][props.level_details.number];
+                    
+                    anime({
+                        targets: bg_placeholder_ref.current,
+                        duration: 500,
+                        opacity: [0, 1],
+                        easing: 'linear',
+                    })
+                }
+            })
+    }, [])
+
     return (
         <div className='level-info-blurred' onClick={(e) => checkCloseCondition(e)} >
             <div className="level-info-box">
-                <div className="level-info-box-content">
+                <div className="level-info-box-content" >
                     <div className='level-info-box-content-item' datatype="info">
                         <div className='content-item-part'>
                             <div className='content-item-serie'> {props.serie_name}  </div>
@@ -69,6 +113,7 @@ function LevelInfo(props) {
                         <FontAwesomeIcon icon={star_empty} className="icon-star_empty"></FontAwesomeIcon>
                     </div>
 
+                    <div className='bg-placeholder' ref={bg_placeholder_ref} ></div>
                 </div>
             </div>
         </div>
---
+++ src/global/exceptions/background_gradients.js
@@ -0,0 +1,13 @@
+
+const background_gradients = {
+    tf: {
+        1: 'linear-gradient(135deg, hsl(0, 0%, 20%), hsl(0, 63%, 48%), hsl(0, 66%, 32%), hsl(0, 63%, 48%), hsl(0, 0%, 20%))',
+        2: 'linear-gradient(40deg, hsl(246, 83%, 60%), hsl(128, 57%, 53%))',
+        3: 'linear-gradient(1deg, hsl(118, 35%, 70%), hsl(118, 35%, 70%))',
+        4: 'repeating-linear-gradient(190deg, hsl(41, 70%, 50%), hsl(50, 70%, 50%) 8%, hsl(0, 0%, 10%) 12%)',
+        5: 'linear-gradient(1deg, #000, #000)',
+        6: 'linear-gradient(132deg, hsl(52, 80%, 60%), hsl(282, 80%, 40%))',
+    }
+}
+
+export  { background_gradients }
