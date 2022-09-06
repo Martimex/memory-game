ea70dd733c19b632d1b0ab2f711fa15e802cc151
Martimex <kamilmarciniak533@wp.pl>
2022-09-06 13:52:39 +0200

    Minor design update for Preview page
    Here is the next part of UI general updates. Following changes are now
    added:
    
    1. Serie box level elements received colorful borders, which consists
    of 4 random colors, that are constantly animating. That is the ultimate
    design for level boxes, however not for Serie box component, since
    there are some animations left to apply:
    
    - Onclick for serie box -> would hide the title name temporarily, then
    make levels boxes visible, and finally the title would appear. Only one
    level box could be visible at the time.
    
    - OnMouseOver for each level box -> this one would make the tile flip
    and show the difficulty level based color. Something similiar to
    the old landing page hover effect.

--- src/components/serie_box.js
+++ src/components/serie_box.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { all_levels } from '../global/all_levels.js';
 import '../styles/serie_box.css';
 import { series_abbr } from '../global/series_abbr.js';
+import { rainbowColors } from '../global/rainbow_colors.js';
+
+import anime from 'animejs/lib/anime.es.js';
+
+const colors_number = 4;
+
+const dynamic_classes = {
+    level_borders: 'mode-block__serie__border',
+} 
+
+let currentlyColoredBorders = [
+
+];
 
 function openUpFire(e) {
-/*     console.log(e.target);
+/*  console.log(e.target);
 
     const serieContent = e.target.querySelector('.serie-content');
     const serieTitle = e.target.querySelector('.serie-title');
@@ -13,6 +26,48 @@ function openUpFire(e) {
 
     serieTitle.classList.add('invisible');
     serieContent.classList.remove('invisible'); */
+
+    const thisSerieTiles = e.target.querySelectorAll(`.${dynamic_classes.level_borders}`);
+    console.log(thisSerieTiles);
+    thisSerieTiles.forEach(level_tile => {
+        currentlyColoredBorders.push([]); // arr for tile
+        const colorsCopy = [...rainbowColors];
+        let gradient_String = '';
+
+        for(let color_no = 0; color_no < colors_number; color_no++) {
+            // 1. Get rand 2. Push 3. Splice
+            let rand = Math.floor(Math.random() * colorsCopy.length);
+            currentlyColoredBorders[currentlyColoredBorders.length -1].push(colorsCopy[rand]);
+            gradient_String+= (color_no !== colors_number - 1)? ` ${colorsCopy[rand]},` : ` ${colorsCopy[rand]}`;
+            colorsCopy.splice(rand, 1);
+        }
+
+        //console.log(gradient_String);
+        level_tile.style.backgroundImage = `linear-gradient(135deg, ${gradient_String})`;
+        //console.log(level_tile.style.backgroundImage);
+        let ccb = currentlyColoredBorders;
+
+        anime({
+            targets: level_tile,
+            duration: 14000,
+            backgroundImage: [
+                `linear-gradient(135deg, ${ccb[ccb.length -1][0]}, ${ccb[ccb.length -1][1]}, ${ccb[ccb.length -1][2]}, ${ccb[ccb.length -1][3]} )`,
+                `linear-gradient(135deg, ${ccb[ccb.length -1][1]}, ${ccb[ccb.length -1][2]}, ${ccb[ccb.length -1][3]}, ${ccb[ccb.length -1][0]} )`,
+                `linear-gradient(135deg, ${ccb[ccb.length -1][2]}, ${ccb[ccb.length -1][3]}, ${ccb[ccb.length -1][0]}, ${ccb[ccb.length -1][1]} )`,
+                `linear-gradient(135deg, ${ccb[ccb.length -1][3]}, ${ccb[ccb.length -1][0]}, ${ccb[ccb.length -1][1]}, ${ccb[ccb.length -1][2]} )`,
+            ],
+            hueRotate: [
+               '0deg', '20deg', '40deg', '60deg', '40deg', '20deg', '0deg'
+            ],
+            saturate: '140%',
+            loop: true,
+            direction: 'alternate',
+            easing: 'linear',
+        })
+    })
+
+
+    console.log(currentlyColoredBorders);
 }
 
 function SerieBox(props) {
@@ -22,14 +77,15 @@ function SerieBox(props) {
             {/* <div className='serie-title'> {series_abbr[props.serie]} </div> */}
             <div className='serie-content invisible'>
                 {Object.keys(all_levels[props.serie]).map((lv, index) =>
-                    <div className='mode-block__serie__level'
-                        key={props.serie.toString() + index.toString()}
-                        onClick={() => {props.setLevelChoose([all_levels[props.serie][lv], props.serie])}}
-                    >
+                    <div className={`${dynamic_classes.level_borders}`} key={props.serie.toString() + index.toString()}>
+                        <div className='mode-block__serie__level'
+                            onClick={() => {props.setLevelChoose([all_levels[props.serie][lv], props.serie])}}
+                        >
 
-                        {all_levels[props.serie][lv].number} 
+                            {(all_levels[props.serie][lv].number < 10)? '0'+all_levels[props.serie][lv].number : all_levels[props.serie][lv].number} 
 
-                    </div>
+                        </div>
+                    </div>   
                 )}
             </div>
         </div>
---
+++ src/global/rainbow_colors.js
@@ -0,0 +1,39 @@
+const saturate = 100;
+const lightness = 62;
+const opacity = 1;
+
+const rainbowColors = [
+    `hsla(0, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(10, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(15, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(20, ${saturate}%, ${lightness}%, ${opacity})`,
+    
+    `hsla(30, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(40, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(45, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(50, ${saturate}%, ${lightness}%, ${opacity})`,
+
+    `hsla(60, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(75, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(90, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(105, ${saturate}%, ${lightness}%, ${opacity})`,
+
+    `hsla(120, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(150, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(180, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(210, ${saturate}%, ${lightness}%, ${opacity})`,
+
+    `hsla(240, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(250, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(260, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(270, ${saturate}%, ${lightness}%, ${opacity})`,
+
+    //`hsla(275, ${saturate}%, ${lightness}%, ${opacity})`,
+
+    `hsla(280, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(295, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(310, ${saturate}%, ${lightness}%, ${opacity})`,
+        //`hsla(325, ${saturate}%, ${lightness}%, ${opacity})`,
+];
+
+export { rainbowColors };
