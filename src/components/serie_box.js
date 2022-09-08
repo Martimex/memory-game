a338d0ae80b3afc656c0422f92c68d6d3397c95a
Martimex <kamilmarciniak533@wp.pl>
2022-09-08 13:54:02 +0200

    Complete serie boxes animations
    New animations has been added to serie boxes. Now player first has to
    pick a serie box, then a levels from chosen serie shows up. Player can
    only see one serie levels' at the time. Also, serie showUp and FadeOut
    are implemented, and will work equally for every future serie box added.

--- src/components/serie_box.js
+++ src/components/serie_box.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { all_levels } from '../global/all_levels.js';
 import '../styles/serie_box.css';
 import { series_abbr } from '../global/series_abbr.js';
@@ -9,6 +9,7 @@ import anime from 'animejs/lib/anime.es.js';
 const colors_number = 4;
 
 const dynamic_classes = {
+    serie_block: 'mode-block__serie',
     level_borders: 'mode-block__serie__border',
     level_tile_main: 'mode-block__serie__tile',
     level_tile_front: 'mode-block__serie__level',
@@ -19,59 +20,9 @@ let currentlyColoredBorders = [
 
 ];
 
-function openUpFire(e) {
-/*  console.log(e.target);
+let recentShowUpBox = null;
 
-    const serieContent = e.target.querySelector('.serie-content');
-    const serieTitle = e.target.querySelector('.serie-title');
-
-    console.log(serieContent, serieTitle)
-
-    serieTitle.classList.add('invisible');
-    serieContent.classList.remove('invisible'); */
-
-    const thisSerieTiles = e.target.querySelectorAll(`.${dynamic_classes.level_borders}`);
-    console.log(thisSerieTiles);
-    thisSerieTiles.forEach(level_tile => {
-        currentlyColoredBorders.push([]); // arr for tile
-        const colorsCopy = [...rainbowColors];
-        let gradient_String = '';
-
-        for(let color_no = 0; color_no < colors_number; color_no++) {
-            // 1. Get rand 2. Push 3. Splice
-            let rand = Math.floor(Math.random() * colorsCopy.length);
-            currentlyColoredBorders[currentlyColoredBorders.length -1].push(colorsCopy[rand]);
-            gradient_String+= (color_no !== colors_number - 1)? ` ${colorsCopy[rand]},` : ` ${colorsCopy[rand]}`;
-            colorsCopy.splice(rand, 1);
-        }
-
-        //console.log(gradient_String);
-        level_tile.style.backgroundImage = `linear-gradient(135deg, ${gradient_String})`;
-        //console.log(level_tile.style.backgroundImage);
-        let ccb = currentlyColoredBorders;
-
-        anime({
-            targets: level_tile,
-            duration: 14000,
-            backgroundImage: [
-                `linear-gradient(135deg, ${ccb[ccb.length -1][0]}, ${ccb[ccb.length -1][1]}, ${ccb[ccb.length -1][2]}, ${ccb[ccb.length -1][3]} )`,
-                `linear-gradient(135deg, ${ccb[ccb.length -1][1]}, ${ccb[ccb.length -1][2]}, ${ccb[ccb.length -1][3]}, ${ccb[ccb.length -1][0]} )`,
-                `linear-gradient(135deg, ${ccb[ccb.length -1][2]}, ${ccb[ccb.length -1][3]}, ${ccb[ccb.length -1][0]}, ${ccb[ccb.length -1][1]} )`,
-                `linear-gradient(135deg, ${ccb[ccb.length -1][3]}, ${ccb[ccb.length -1][0]}, ${ccb[ccb.length -1][1]}, ${ccb[ccb.length -1][2]} )`,
-            ],
-            hueRotate: [
-               '0deg', '20deg', '40deg', '60deg', '40deg', '20deg', '0deg'
-            ],
-            saturate: '140%',
-            loop: true,
-            direction: 'alternate',
-            easing: 'linear',
-        })
-    })
-
-
-    console.log(currentlyColoredBorders);
-}
+let animationFinishController = [true, true];
 
 function flipTile(e, mouseEventName) {
 
@@ -110,9 +61,155 @@ function flipTile(e, mouseEventName) {
 
 function SerieBox(props) {
 
+    function openUpFire(e) {
+        //console.warn(e.target.classList);
+        if(!animationFinishController[0] || !animationFinishController[1]) return; // Dont fire if both animations are not completed
+        if(e.target === recentShowUpBox) return; // Dont fire if old and new sections are the same
+        if(!e.target.classList.contains(`${dynamic_classes.serie_block}`)) return; // Only section block can be targeted
+        const thisSerieTiles = e.target.querySelectorAll(`.${dynamic_classes.level_borders}`);
+        console.log(thisSerieTiles);
+        thisSerieTiles.forEach(level_tile => {
+            currentlyColoredBorders.push([]); // arr for tile
+            const colorsCopy = [...rainbowColors];
+            let gradient_String = '';
+    
+            for(let color_no = 0; color_no < colors_number; color_no++) {
+                // 1. Get rand 2. Push 3. Splice
+                let rand = Math.floor(Math.random() * colorsCopy.length);
+                currentlyColoredBorders[currentlyColoredBorders.length -1].push(colorsCopy[rand]);
+                gradient_String+= (color_no !== colors_number - 1)? ` ${colorsCopy[rand]},` : ` ${colorsCopy[rand]}`;
+                colorsCopy.splice(rand, 1);
+            }
+    
+    
+            level_tile.style.backgroundImage = `linear-gradient(135deg, ${gradient_String})`;
+            let ccb = currentlyColoredBorders;
+    
+            anime({
+                targets: level_tile,
+                duration: 14000,
+                backgroundImage: [
+                    `linear-gradient(135deg, ${ccb[ccb.length -1][0]}, ${ccb[ccb.length -1][1]}, ${ccb[ccb.length -1][2]}, ${ccb[ccb.length -1][3]} )`,
+                    `linear-gradient(135deg, ${ccb[ccb.length -1][1]}, ${ccb[ccb.length -1][2]}, ${ccb[ccb.length -1][3]}, ${ccb[ccb.length -1][0]} )`,
+                    `linear-gradient(135deg, ${ccb[ccb.length -1][2]}, ${ccb[ccb.length -1][3]}, ${ccb[ccb.length -1][0]}, ${ccb[ccb.length -1][1]} )`,
+                    `linear-gradient(135deg, ${ccb[ccb.length -1][3]}, ${ccb[ccb.length -1][0]}, ${ccb[ccb.length -1][1]}, ${ccb[ccb.length -1][2]} )`,
+                ],
+                hueRotate: [
+                   '0deg', '20deg', '40deg', '60deg', '40deg', '20deg', '0deg'
+                ],
+                saturate: '140%',
+                loop: true,
+                direction: 'alternate',
+                easing: 'linear',
+            })
+        })
+    
+        animateSectionShowUp(e);
+        animateOtherSectionHide(e);
+    }
+    
+    function animateSectionShowUp(e) {
+        animationFinishController[0] = false;
+        const section_title = e.target.querySelector(`.serie-title`);
+        const section_content = e.target.querySelector(`.serie-content`);
+        const section_content_tiles = section_content.querySelectorAll(`.${dynamic_classes.level_borders}`);
+    
+        section_content.classList.remove('invisible');
+       
+        section_content_tiles.forEach(lv_tile => lv_tile.style.pointerEvents = 'auto');
+    
+        async function animationChain() {
+            await fadeText()
+            .then(() => {
+                section_title.classList.add('invisible');
+            })
+            await showUpLevels()
+            .then(() => {
+                animationFinishController[0] = true;
+            })
+        }
+    
+        async function fadeText() {
+            const a1 = anime({
+                targets: section_title,
+                duration: 650,
+                translateX: '30%',
+                filter: 'sepia(70%)',
+                opacity: 0,
+                easing: 'easeOutSine',
+            }).finished;
+            await Promise.all([a1]);
+        }
+    
+        async function showUpLevels() {
+            const a2 = anime({
+                targets: section_content_tiles,
+                duration: 500,
+                delay: anime.stagger(120),
+                opacity: 1,
+                easing: 'easeInCubic',
+            }).finished;
+            await Promise.all([a2]);
+        }
+    
+        animationChain();
+    }
+    
+    function animateOtherSectionHide(e) {
+
+        if(recentShowUpBox) {
+            animationFinishController[1] = false;
+            console.log(recentShowUpBox);
+            const section_title_old = recentShowUpBox.querySelector(`.serie-title`);
+            const section_content_old = recentShowUpBox.querySelector(`.serie-content`);
+            const section_content_tiles_old = section_content_old.querySelectorAll(`.${dynamic_classes.level_borders}`);
+
+            section_content_tiles_old.forEach(lv_tile => lv_tile.style.pointerEvents = 'none');
+
+            async function animationChain() {
+                section_title_old.classList.remove('invisible');
+                await hideLevels()
+                await showText()
+                .then(() => {
+                    section_content_old.classList.add('invisible');
+                    animationFinishController[1] = true;
+                })
+            }
+
+            async function hideLevels() {
+                const a1 = anime({
+                    targets: section_content_tiles_old,
+                    duration: 500,
+                    delay: anime.stagger(120, {from: 'last'}),
+                    opacity: 0,
+                    easing: 'easeOutCubic',
+                }).finished;
+                await Promise.all([a1]);
+            }
+
+            async function showText() {
+                const a2 = anime({
+                    targets: section_title_old,
+                    duration: 650,
+                    translateX: 0,
+                    filter: 'sepia(0%)',
+                    opacity: 1,
+                    easing: 'easeInSine',
+                }).finished;
+                await Promise.all([a2]);
+            }
+
+            animationChain(section_content_tiles_old);
+
+        }
+
+        recentShowUpBox = e.target;
+    }
+    
+
     return(
-        <div className='mode-block__serie' onClick={(e) => openUpFire(e)} onMouseOver={(e) => flipTile(e, 'over')} onMouseOut={(e) => {flipTile(e, 'out')}} >
-            {/* <div className='serie-title'> {series_abbr[props.serie]} </div> */}
+        <div className={`${dynamic_classes.serie_block}`} onClick={(e) => {openUpFire(e)} } onMouseOver={(e) => flipTile(e, 'over')} onMouseOut={(e) => {flipTile(e, 'out')}} >
+            <div className='serie-title'> {series_abbr[props.serie]} </div> 
             <div className='serie-content invisible'>
                 {Object.keys(all_levels[props.serie]).map((lv, index) =>
                     <div className={`${dynamic_classes.level_borders}`} key={props.serie.toString() + index.toString()}
