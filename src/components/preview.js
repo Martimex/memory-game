f885e231dac73f08dc48305111dca25935ecaa71
Martimex <kamilmarciniak533@wp.pl>
2022-09-11 20:03:21 +0200

    Finish sticky topbar for Preview page
    Sticky topbar is now implemented for preview page. It has added
    transitions during activation and deactivation. Works fine across all
    browsers.

--- src/components/preview.js
+++ src/components/preview.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import '../styles/preview.css';
 import anime from 'animejs/lib/anime.es.js';
 
@@ -13,11 +13,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function Preview(props) {
 
     const [[levelChoose, serie], setLevelChoose] = useState([null, null]);
-    const [chosenSerieName, setChosenSerieName] = useState('Choose a serie');
+    const [chosenSerie, setChosenSerie] = useState(null);
+    const [chosenSerieName, setChosenSerieName] = useState(null);
     const animationTextOpacity = React.useRef(null);
 
+    const currSerie = useRef(null);
+    const topBarRef = useRef(null);
+
     const levels_showcase = Object.keys(all_levels).map((serie_name, index) => 
-        <SerieBox serie={serie_name} setSerieName={setChosenSerieName} setLevelChoose={setLevelChoose} key={serie_name.toString()} />
+        <SerieBox serie={serie_name} setSerieName={setChosenSerie} setLevelChoose={setLevelChoose} key={serie_name.toString()} />
     )
 
     useEffect(() => {
@@ -85,12 +89,78 @@ function Preview(props) {
         await fadeAnimation()
     }
 
+    useEffect(() => {
+        async function fade() {
+            await animate()
+                .then(() => setChosenSerieName(chosenSerie))
+        }
+        
+        async function animate() {
+            const a1 = anime({
+                targets: currSerie.current,
+                duration: 500,
+                opacity: [1, 0],
+                easing: 'linear',
+            }).finished;
+
+            await Promise.all([a1]);
+        }
+
+        fade();
+
+    }, [chosenSerie])
+
+    useEffect(() => {
+        anime({
+            targets: currSerie.current,
+            duration: 500,
+            opacity: [0, 1],
+            easing: 'linear',
+        })
+    }, [chosenSerieName])
     //console.log(all_levels['tf'][2].limitations) - here some props can be accessed
+
+    useEffect(() => {
+        window.addEventListener('scroll', checkStickyBar); 
+
+        return () => {
+            window.removeEventListener('scroll', checkStickyBar);
+        }
+    });
+
+    function checkStickyBar() {
+        //const sticky = topBarRef.current.offsetTop;
+        const isSticky_before = topBarRef.current.classList.contains('sticky');
+        topBarRef.current.classList.toggle('sticky', window.scrollY > 0);
+        const isSticky_after = topBarRef.current.classList.contains('sticky');
+
+        if(isSticky_before !== isSticky_after) {
+            if(isSticky_after === true) {
+                // Enter sticky mode
+                anime({
+                    targets: topBarRef.current,
+                    duration: 500,
+                    backgroundColor: ['#2223', '#222f'],
+                    easing: 'linear',
+                })
+            }
+
+            else {
+                // Close sticky mode
+                anime({
+                    targets: topBarRef.current,
+                    duration: 500,
+                    backgroundColor: ['#222f', '#2223'],
+                    easing: 'linear',
+                })
+            }
+        }
+    }
 
     return (
         <div className='bg-main'>
             <div className='seizure-flexbox'>
-                <div className='top-bar'>
+                <div className='top-bar' ref={topBarRef}>
                     <div className='top-bar__return'>
                         <button className='return-back' onClick={() => {props.backToHome(); proceed();}}> 
                             <FontAwesomeIcon icon={home} className="icon-home" />
@@ -98,7 +168,7 @@ function Preview(props) {
                     </div>
 
                     <div className='top-bar__title'>
-                        <div className='title-name'> {chosenSerieName} </div>
+                        <div className='title-name' ref={currSerie}> {chosenSerieName ? chosenSerieName : `Choose a serie` } </div>
                     </div>
 
                     <div className='top-bar__follow'>
